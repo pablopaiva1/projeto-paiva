c65432b7fbfefb073092f880139d2abbcccde81f
Thales Rodrigues <thalesresilva@gmail.com>
2024-07-03 11:25:09 -0300

    Update user.js

--- user.js
+++ user.js
@@ -3,8 +3,4 @@ const senha = "12576"
 const token = "snkkajsdbkjbdfkjvkfjdpfionowren"
 
 const users = [email,senha,token];
-pablo esteve aqui...........................
-  
-
-
-wg,[pq3y  24uuj4444=lq3ui=o7 iivki-yu24i24yu39k136136136136136136I0136913TYk=013y62029´p2u u u u u u u u u u u u u u u u u u u u u i  2
+//pablo esteve aqui...........................
